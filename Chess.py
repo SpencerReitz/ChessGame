e7a2b91bed496a4a85dbd4df2ff96dca3343b23f
Spencer Reitz <sreitz@pop-os.localdomain>
2021-09-18 14:09:04 -0600

    Moved piece functions to Piece.py

--- Chess.py
+++ Chess.py
@@ -1,5 +1,6 @@
 import pygame
 import pygame.image
+from Piece import Knight
 
 ## Makes the game screen and names the window
 pygame.init()
@@ -15,66 +16,12 @@ backgroundColor = (102, 178, 255)
 # Initializes the piece images
 BK = pygame.image.load("BlackKing.png")
 BK = pygame.transform.scale(BK, (100, 100)) 
-BQ = pygame.image.load("BlackQueen.png")
-BQ = pygame.transform.scale(BQ, (100, 100)) 
-BB = pygame.image.load("BlackBishop.png")
-BB = pygame.transform.scale(BB, (100, 100))
-BP = pygame.image.load("BlackPawn.png")
-BP = pygame.transform.scale(BP, (100, 100))
-BN = pygame.image.load("BlackKnight.png")
-BN = pygame.transform.scale(BN, (100, 100))
-BR = pygame.image.load("BlackRook.png")
-BR = pygame.transform.scale(BR, (100, 100))
-
-WK = pygame.image.load("WhiteKing.png")
-WK = pygame.transform.scale(WK, (100, 100))
-WQ = pygame.image.load("WhiteQueen.png")
-WQ = pygame.transform.scale(WQ, (100, 100))
-WB = pygame.image.load("WhiteBishop.png")
-WB = pygame.transform.scale(WB, (100, 100))
-WP = pygame.image.load("WhitePawn.png")
-WP = pygame.transform.scale(WP, (100, 100))
-WN = pygame.image.load("WhiteKnight.png")
-WN = pygame.transform.scale(WN, (100, 100))
-WR = pygame.image.load("WhiteRook.png")
-WR = pygame.transform.scale(WR, (100, 100))
-
 
 # Just places the base game
 def initialGameState():
-    screen.blit(BR, (100, 100))
-    screen.blit(BN, (200, 100))
-    screen.blit(BB, (300, 100))
-    screen.blit(BQ, (400, 100))
-    screen.blit(BK, (500, 100))
-    screen.blit(BB, (600, 100))
-    screen.blit(BN, (700, 100))
-    screen.blit(BR, (800, 100))
-    screen.blit(BP, (100, 200))
-    screen.blit(BP, (200, 200))
-    screen.blit(BP, (300, 200))
-    screen.blit(BP, (400, 200))
-    screen.blit(BP, (500, 200))
-    screen.blit(BP, (600, 200))
-    screen.blit(BP, (700, 200))
-    screen.blit(BP, (800, 200))
-
-    screen.blit(WR, (100, 800))
-    screen.blit(WN, (200, 800))
-    screen.blit(WB, (300, 800))
-    screen.blit(WQ, (400, 800))
-    screen.blit(WK, (500, 800))
-    screen.blit(WB, (600, 800))
-    screen.blit(WN, (700, 800))
-    screen.blit(WR, (800, 800))
-    screen.blit(WP, (100, 700))
-    screen.blit(WP, (200, 700))
-    screen.blit(WP, (300, 700))
-    screen.blit(WP, (400, 700))
-    screen.blit(WP, (500, 700))
-    screen.blit(WP, (600, 700))
-    screen.blit(WP, (700, 700))
-    screen.blit(WP, (800, 700))
+    screen.blit(BK, (100, 100))
+    BN = Knight('BlackKnight.png', (100, 100), 'black', screen)
+    BN.draw()
 
 # Callable funition that will draw an 8x8 chess board with grey and white squares and a blue background
 def boardCreation():
@@ -102,6 +49,8 @@ def boardCreation():
 
 # Starts the game
 game = True
+
+
 
 # Main game loop
 while game:
---
+++ Piece.py
@@ -0,0 +1,16 @@
+import pygame
+import pygame.image
+
+class Piece:
+    def __init__(self, image, location, color, screen):
+        self.image    = image
+        self.location = location
+        self.color    = color
+        self.screen = screen
+    def draw(self):
+        pieceImage = pygame.image.load(self.image)
+        scaledPieceImage = pygame.transform.scale(pieceImage, (100, 100))
+        self.screen.blit(scaledPieceImage, self.location)
+
+class Knight(Piece):
+    pass
