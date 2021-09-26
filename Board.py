2a6ec95d4d61e527e557542310ba77246e48620b
Spencer Reitz <spencerreitz2002@gmail.com>
2021-09-25 19:34:53 -0600

    Changed the White pieces to bottom and added the black pieces

--- Board.py
+++ Board.py
@@ -11,15 +11,25 @@ class Board():
                 new.append(None)
             self.board.append(new)
 
-        self.board[0][0] = Rook("WhiteRook", (0, 0), 'w')
-        self.board[0][7] = Rook("WhiteRook", (0, 7), 'w')
-        self.board[0][1] = Knight("WhiteKnight", (0, 1), 'w')
-        self.board[0][6] = Knight("WhiteKnight", (0, 6), 'w')
-        self.board[0][2] = Bishop("WhiteBishop", (0, 2), 'w')
-        self.board[0][5] = Bishop("WhiteBishop", (0, 5), 'w')
-        self.board[0][3] = King("WhiteKing", (0, 3), 'w')
-        self.board[0][4] = Queen("WhiteQueen", (0, 4), 'w')
-        self.board[1] = [Pawn("WhitePawn", (1, x), 'w') for x in range(8)]
+        self.board[7][0] = Rook("WhiteRook", (7, 0), 'w')
+        self.board[7][7] = Rook("WhiteRook", (7, 7), 'w')
+        self.board[7][1] = Knight("WhiteKnight", (7, 1), 'w')
+        self.board[7][6] = Knight("WhiteKnight", (7, 6), 'w')
+        self.board[7][2] = Bishop("WhiteBishop", (7, 2), 'w')
+        self.board[7][5] = Bishop("WhiteBishop", (7, 5), 'w')
+        self.board[7][4] = King("WhiteKing", (7, 4), 'w')
+        self.board[7][3] = Queen("WhiteQueen", (7, 3), 'w')
+        self.board[6] = [Pawn("WhitePawn", (6, x), 'w') for x in range(8)]
+
+        self.board[0][0] = Rook("BlackRook", (0, 0), 'b')
+        self.board[0][7] = Rook("BlackRook", (0, 7), 'b')
+        self.board[0][1] = Knight("BlackKnight", (0, 1), 'b')
+        self.board[0][6] = Knight("BlackKnight", (0, 6), 'b')
+        self.board[0][2] = Bishop("BlackBishop", (0, 2), 'b')
+        self.board[0][5] = Bishop("BlackBishop", (0, 5), 'b')
+        self.board[0][4] = King("BlackKing", (0, 4), 'b')
+        self.board[0][3] = Queen("BlackQueen", (0, 3), 'b')
+        self.board[1] = [Pawn("BlackPawn", (1, x), 'b') for x in range(8)]
 
     def __init__(self):
         self.__build_board()
